d5c3b1f4a233cd06d0bbb7a953eecd7331332fb1
AdaMaldonado <34352705+AdaMaldonado@users.noreply.github.com>
2022-01-07 14:35:52 -0500

    Answer to question 2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let containerEl = document.getElementById("container");
 
 // 2 - Select the section with an id of container using querySelector.
-
+let containerEl2 = document.querySelector("#container);
 
 // 3 - Select all of the list items with a class of "second".
 
